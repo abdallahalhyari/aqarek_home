990adbca9725c27505654974f2d7331e16fc5ee0
qaistayeh <q.tayeh@gmail.com>
2023-12-05 22:21:22 +0300

    -

--- lib/module/home_page/bloc/home_page_bloc.dart
+++ lib/module/home_page/bloc/home_page_bloc.dart
@@ -1,19 +1,24 @@
+import 'package:aqarek_home/model/home_page_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 // ignore: depend_on_referenced_packages
 import 'package:meta/meta.dart';
+
+import '../data/repository.dart';
 part 'home_page_event.dart';
 part 'home_page_state.dart';
 
-// class FamilyHistoryFhxBloc
-//     extends Bloc<FamilyHistoryFhxEvent, FamilyHistoryFhxState> {
-//   // final RepositoryFamilyHistoryFHx repositoryFamilyHistoryFHx =
-//   //     RepositoryFamilyHistoryFHx();
-//   // List<FamilyHistoryFHxModel> familyHistoryFHx = [];
-//   // FamilyHistoryFhxBloc() : super(FamilyHistoryFhxInitial()) {
-//   //   on<LoadFamilyHistoryFhxEvent>((event, emit) async {
-//   //     familyHistoryFHx =
-//   //         (await repositoryFamilyHistoryFHx.agetFamilyHistoryFHx())!;
-//   //     emit(LoadedFamilyHistoryFhxState());
-//   //   });
-//   // }
-// }
+class HomePageBloc
+    extends Bloc<HomePageEvent, HomePageState> {
+  final Repository repository =
+      Repository();
+  HomePageModel? homePageModel ;
+  HomePageBloc() : super(HomePageInitial()) {
+    on<LoadHomePageEvent>((event, emit) async {
+      emit(LoadingHomePageState());
+      homePageModel =
+          await repository.getData();
+      emit(LoadedHomePageState());
+    });
+  }
+}
+
