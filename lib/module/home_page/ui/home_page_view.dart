28576e02dbb8446a59d5ea740682de7fe2d91fb3
qaistayeh <q.tayeh@gmail.com>
2023-12-06 22:04:50 +0300

    -

--- lib/module/home_page/ui/home_page_view.dart
+++ lib/module/home_page/ui/home_page_view.dart
@@ -7,7 +7,6 @@ import 'package:aqarek_home/module/home_page/ui/widgets/latest_projects_widget.d
 import 'package:aqarek_home/module/home_page/ui/widgets/offers_widget.dart';
 import 'package:aqarek_home/module/home_page/ui/widgets/units_items_widget.dart';
 import 'package:aqarek_home/utils/constant/path_icons.dart';
-import 'package:aqarek_home/utils/constant/path_svg.dart';
 import 'package:aqarek_home/widget/text/custom_text_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -22,7 +21,7 @@ class HomePageView extends StatefulWidget {
 
 class _HomePageViewState extends State<HomePageView> {
   final _pageController = PageController(initialPage: 4);
-  final _controller = NotchBottomBarController(index: 4);
+  //final _controller = NotchBottomBarController(index: 4);
   int maxCount = 5;
 
   @override
@@ -49,100 +48,90 @@ class _HomePageViewState extends State<HomePageView> {
                 bottomBarPages.length, (index) => bottomBarPages[index])),
         extendBody: true,
         bottomNavigationBar: (bottomBarPages.length <= maxCount)
-            ? Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Container(
-                          color: const Color.fromRGBO(20, 76, 237, 1.0),
-                          width: MediaQuery.sizeOf(context).width,
-                          height: 45)),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      AnimatedNotchBottomBar(
-                        itemLabelStyle:
-                            const TextStyle(color: Colors.white, fontSize: 12),
-                        showShadow: false,
-                        notchBottomBarController:
-                            NotchBottomBarController(index: 4),
+            ? Stack(alignment: Alignment.bottomCenter, children: [
+                Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
                         color: const Color.fromRGBO(20, 76, 237, 1.0),
-                        showLabel: true,
-                        notchColor: Colors.transparent,
-                        removeMargins: true,
-                        bottomBarWidth: MediaQuery.sizeOf(context).width,
-                        durationInMilliSeconds: 300,
-                        bottomBarItems: [
-                          BottomBarItem(
-                              inActiveItem: PathSvg.category,
-                              activeItem: PathSvg.category,
-                              itemLabel: 'أخرى'),
-                          BottomBarItem(
-                              inActiveItem: SvgPicture.asset(PathIcons.orders,
-                                  fit: BoxFit.cover,
-                                  // ignore: deprecated_member_use
-                                  color: Colors.grey),
-                              activeItem: SvgPicture.asset(
-                                PathIcons.orders,
-                                fit: BoxFit.cover,
-                                // ignore: deprecated_member_use
-                                color: const Color.fromRGBO(20, 76, 237, 1.0),
-                              ),
-                              itemLabel: 'طلباتي'),
-                          BottomBarItem(
-                              inActiveItem: SvgPicture.asset(PathIcons.map,
-                                  fit: BoxFit.cover,
-                                  // ignore: deprecated_member_use
-                                  color: Colors.grey),
-                              activeItem: SvgPicture.asset(
-                                PathIcons.map,
-                                fit: BoxFit.cover,
-                                // ignore: deprecated_member_use
-                                color: const Color.fromRGBO(20, 76, 237, 1.0),
-                              ),
-                              itemLabel: 'خريطة'),
-                          BottomBarItem(
-                              inActiveItem: PathSvg.search,
-                              activeItem: PathSvg.search,
-                              itemLabel: 'بحث'),
-                          BottomBarItem(
-                              inActiveItem: SvgPicture.asset(
-                                PathIcons.others,
-                                fit: BoxFit.cover,
-                                //color: Colors.grey,
-                              ),
-                              // Icon(Icons.person, color: Colors.blueGrey),
-                              activeItem: SvgPicture.asset(
-                                PathIcons.others,
-                                fit: BoxFit.cover,
-                                //color: const Color.fromRGBO(20, 76, 237, 1.0),
-                              ),
-                              //Icon(Icons.person, color: Colors.yellow),
-                              itemLabel: 'الرئيسية')
-                        ],
-                        onTap: (index) {
-                          // log('current selected index $index');
-                          _pageController.jumpToPage(index);
-                        },
-                      ),
-                      Container(
                         width: MediaQuery.sizeOf(context).width,
-                        color: const Color.fromRGBO(20, 76, 237, 1.0),
-                        height: 20,
-                        child: const Row(children: [
-                          Spacer(),
-                          TextViewCustom(
-                              size: 14, text: 'الرئيسية', color: Colors.white),
-                          SizedBox(
-                            width: 35,
-                          ),
-                        ]),
-                      )
-                    ],
-                  ),
-                ],
-              )
+                        height: 45)),
+                Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+                  AnimatedNotchBottomBar(
+                      itemLabelStyle:
+                          const TextStyle(color: Colors.white, fontSize: 12),
+                      showShadow: false,
+                      notchBottomBarController:
+                          NotchBottomBarController(index: 4),
+                      color: const Color.fromRGBO(20, 76, 237, 1.0),
+                      showLabel: true,
+                      notchColor: Colors.transparent,
+                      removeMargins: true,
+                      bottomBarWidth: MediaQuery.sizeOf(context).width,
+                      durationInMilliSeconds: 300,
+                      bottomBarItems: [
+                        BottomBarItem(
+                            inActiveItem: SvgPicture.asset(PathIcons.category,
+                                fit: BoxFit.contain,
+                                // ignore: deprecated_member_use
+                                color: Colors.grey),
+                            activeItem: SvgPicture.asset(PathIcons.category,
+                                fit: BoxFit.contain),
+                            itemLabel: 'أخرى'),
+                        BottomBarItem(
+                            inActiveItem: SvgPicture.asset(PathIcons.orders,
+                                fit: BoxFit.cover,
+                                // ignore: deprecated_member_use
+                                color: Colors.grey),
+                            activeItem: SvgPicture.asset(
+                              PathIcons.orders,
+                              fit: BoxFit.cover,
+                              // ignore: deprecated_member_use
+                              color: const Color.fromRGBO(20, 76, 237, 1.0),
+                            ),
+                            itemLabel: 'طلباتي'),
+                        BottomBarItem(
+                            inActiveItem: SvgPicture.asset(PathIcons.map,
+                                fit: BoxFit.cover,
+                                // ignore: deprecated_member_use
+                                color: Colors.grey),
+                            activeItem: SvgPicture.asset(PathIcons.map,
+                                fit: BoxFit.cover,
+                                // ignore: deprecated_member_use
+                                color: const Color.fromRGBO(20, 76, 237, 1.0)),
+                            itemLabel: 'خريطة'),
+                        BottomBarItem(
+                            inActiveItem: SvgPicture.asset(PathIcons.search,
+                                fit: BoxFit.fitHeight,
+                                // ignore: deprecated_member_use
+                                color: Colors.grey),
+                            activeItem: SvgPicture.asset(PathIcons.search,
+                                fit: BoxFit.fitHeight),
+                            itemLabel: 'بحث'),
+                        BottomBarItem(
+                          inActiveItem: SizedBox(
+                              child: SvgPicture.asset(PathIcons.others,
+                                  fit: BoxFit.cover)),
+                          activeItem: SizedBox(
+                              child: SvgPicture.asset(PathIcons.others,
+                                  fit: BoxFit.cover)),
+                          // itemLabel: 'الرئيسية'
+                        )
+                      ],
+                      onTap: (index) {
+                        // _pageController.jumpToPage(4);
+                      }),
+                  Container(
+                      width: MediaQuery.sizeOf(context).width,
+                      color: const Color.fromRGBO(20, 76, 237, 1.0),
+                      height: 20,
+                      child: const Row(children: [
+                        Spacer(),
+                        TextViewCustom(
+                            size: 14, text: 'الرئيسية', color: Colors.white),
+                        SizedBox(width: 35)
+                      ]))
+                ])
+              ])
             : null);
   }
 }
@@ -194,7 +183,7 @@ class _Page1State extends State<Page1> {
                                     homePageBloc.homePageModel!.units!.items!),
                             const LastSeenWidget(),
                           ])),
-                     const SizedBox(height: 100)
+                      const SizedBox(height: 100)
                     ])))
             : const Center(child: CircularProgressIndicator());
       },
