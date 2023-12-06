b03a5876b640dbd1efa41d7048684b5c3f55b15d
abdallahalhyari <alhyariabdallh@gmail.com>
2023-12-06 22:55:21 +0300

    first commit

--- lib/module/home_page/ui/widgets/latest_projects_widget.dart
+++ lib/module/home_page/ui/widgets/latest_projects_widget.dart
@@ -1,5 +1,6 @@
 import 'package:aqarek_home/model/home_page_model.dart';
 import 'package:aqarek_home/widget/text/custom_text_view.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class LatestprojectWidget extends StatelessWidget {
@@ -8,7 +9,7 @@ class LatestprojectWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        height: 200,
+        height: 300,
         child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
           const Padding(
               padding: EdgeInsets.only(right: 15.0),
@@ -23,19 +24,25 @@ class LatestprojectWidget extends StatelessWidget {
                   scrollDirection: Axis.horizontal,
                   itemCount: latestProjectsItems?.length,
                   // homePageBloc.homePageModel!.latestProjects!.items!.length,
-                  itemBuilder: (context, index) => Container(
-                      decoration: BoxDecoration(
-                          color: const Color.fromRGBO(20, 76, 237, 1.0),
-                          borderRadius: BorderRadius.circular(15),
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                  latestProjectsItems?[index].image ?? ''),
-                              fit: BoxFit.fill)),
-                      margin: const EdgeInsets.all(15),
-                      height: 190,
-                      width: 130
-                      // child: Image.network(homePageBloc.homePageModel!.latestProjects!.items![index].image??'',fit: BoxFit.fill),
-                      )))
+                  itemBuilder: (context, index) => Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                          decoration: BoxDecoration(
+                              color: const Color.fromRGBO(20, 76, 237, 1.0),
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      latestProjectsItems?[index].image ?? ''),
+                                  fit: BoxFit.fill)),
+                          margin: const EdgeInsets.all(15),
+                          height: 270,
+                          width: 150
+                          // child: Image.network(homePageBloc.homePageModel!.latestProjects!.items![index].image??'',fit: BoxFit.fill),
+                          ),
+                        Positioned(bottom: 1,child: Container(height: 70,width: 150,decoration: BoxDecoration(border:Border.all(color: Colors.grey.shade400),color: Colors.white, borderRadius: BorderRadius.vertical(bottom:  Radius.circular(15)),),))
+                    ],
+                  )))
         ]));
   }
 }
