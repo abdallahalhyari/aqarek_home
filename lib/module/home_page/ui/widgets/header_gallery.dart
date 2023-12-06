2c63055130a28491fc9451f6167f4c970c2d1578
abdallahalhyari <alhyariabdallh@gmail.com>
2023-12-06 21:59:25 +0300

    first commit

--- lib/module/home_page/ui/widgets/header_gallery.dart
+++ lib/module/home_page/ui/widgets/header_gallery.dart
@@ -2,31 +2,61 @@ import 'package:aqarek_home/model/home_page_model.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-class HeaderGalleryWidget extends StatelessWidget {
+class HeaderGalleryWidget extends StatefulWidget {
   const HeaderGalleryWidget({super.key, required this.headerGallery});
   final List<HeaderGallery> headerGallery;
+
+  @override
+  State<HeaderGalleryWidget> createState() => _HeaderGalleryWidgetState();
+}
+
+class _HeaderGalleryWidgetState extends State<HeaderGalleryWidget> {
+  int index=0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: MediaQuery.sizeOf(context).height / 2.4,
-        color: const Color.fromRGBO(20, 76, 237, 1.0),
-        padding: const EdgeInsets.only(bottom: 20),
-        child: CarouselSlider(
-            items: headerGallery
-                .map((e) => SizedBox(
-                    width: MediaQuery.sizeOf(context).width,
-                    child: Image.network(
-                      e.image ?? '',
-                      fit: BoxFit.fill,
-                    )))
-                .toList(),
-            options: CarouselOptions(
-                autoPlay: true,
-                height: double.infinity,
-                enlargeCenterPage: true,
-                viewportFraction: 1,
-                aspectRatio: 1
-                // initialPage: 2,
-                )));
+    return Stack(
+      children: [
+        Container(
+            height: MediaQuery.sizeOf(context).height / 2,
+            color: const Color.fromRGBO(20, 76, 237, 1.0),
+            padding: const EdgeInsets.only(bottom: 20),
+            child: CarouselSlider(
+                items: widget.headerGallery
+                    .map((e) => SizedBox(
+                        width: MediaQuery.sizeOf(context).width,
+                        child: Image.network(
+                          e.image ?? '',
+                          fit: BoxFit.fill,
+                        )))
+                    .toList(),
+                options: CarouselOptions(
+                  onPageChanged: (i,c){
+                    index=i;
+                    setState(() {
+
+                    });
+                  },
+                    autoPlay: true,
+                    height: double.infinity,
+                    enlargeCenterPage: true,
+                    viewportFraction: 1,
+                    aspectRatio: 1
+                    // initialPage: 2,
+                    ))),
+        Positioned(
+          bottom: 20,
+
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            height: 35,
+            child: ListView.builder(shrinkWrap: true,scrollDirection: Axis.horizontal,itemCount: widget.headerGallery.length,itemBuilder:(context,i)=>
+                Container(padding:index==i? const EdgeInsets.symmetric(horizontal: 10):const EdgeInsets.all(3),margin:const EdgeInsets.all(5),decoration: BoxDecoration(color:index==i?Colors.white:Colors.grey.shade300,shape:index==i?BoxShape.rectangle:BoxShape.circle),)
+            ),
+          ),
+        ),
+
+      ],
+    );
   }
 }
--- lib/module/home_page/ui/widgets/offers_widget.dart
+++ lib/module/home_page/ui/widgets/offers_widget.dart
@@ -1,32 +1,59 @@
 import 'package:aqarek_home/model/home_page_model.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class OffersWidget extends StatelessWidget {
-  const OffersWidget({super.key, required this.headerOffers});
+class OffersWidget extends StatefulWidget {
+   OffersWidget({super.key, required this.headerOffers});
 
   final List<HeaderOffers> headerOffers;
 
   @override
+  State<OffersWidget> createState() => _OffersWidgetState();
+}
+
+class _OffersWidgetState extends State<OffersWidget> {
+  int index=0;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 160,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(15)),
-        child: CarouselSlider(
-            items: headerOffers
-                .map((e) => Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        image: DecorationImage(
-                            image: NetworkImage(e.image ?? ''),
-                            fit: BoxFit.fill)),
-                    width: MediaQuery.sizeOf(context).width))
-                .toList(),
-            options: CarouselOptions(
-                height: double.infinity,
-                autoPlay: true,
-                enlargeCenterPage: true,
-                viewportFraction: 1,
-                aspectRatio: 1)));
+    return Column(
+      children: [
+        Container(
+          height: 160,
+          padding: const EdgeInsets.all(10),
+          child: CarouselSlider(
+              items: widget.headerOffers
+                  .map((e) => Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          image: DecorationImage(
+                              image: NetworkImage(e.image ?? ''),
+                              fit: BoxFit.fill)),
+                      width: MediaQuery.sizeOf(context).width))
+                  .toList(),
+              options: CarouselOptions(
+                onPageChanged: (i, c){
+                  index=i;
+                  setState(() {
+
+                  });
+                },
+                  height: double.infinity,
+                  autoPlay: true,
+                  enlargeCenterPage: true,
+                  viewportFraction: 1,
+                  aspectRatio: 1)),
+        ),
+        Container(
+          padding: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          height: 35,
+          child: ListView.builder(shrinkWrap: true,scrollDirection: Axis.horizontal,itemCount: widget.headerOffers.length,itemBuilder:(context,i)=>
+          Container(padding:index==i? const EdgeInsets.symmetric(horizontal: 10):const EdgeInsets.all(3),margin: EdgeInsets.all(5),decoration: BoxDecoration(color:index==i?const Color.fromRGBO(20, 76, 237, 1.0):Colors.grey.shade500,shape:index==i?BoxShape.rectangle:BoxShape.circle),)
+          ),
+        )
+      ],
+    );
   }
 }
