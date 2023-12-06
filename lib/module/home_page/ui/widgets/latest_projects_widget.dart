4e83e01911ee3d31b4f4ff806e5c1dd146b36eb8
qaistayeh <q.tayeh@gmail.com>
2023-12-06 22:52:31 +0300

    -

--- lib/module/home_page/ui/widgets/latest_projects_widget.dart
+++ lib/module/home_page/ui/widgets/latest_projects_widget.dart
@@ -13,50 +13,65 @@ class LatestprojectWidget extends StatelessWidget {
         child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
           const Padding(
               padding: EdgeInsets.only(right: 15.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Icon(Icons.arrow_drop_down,size: 40,),
-                  TextViewCustom(
-                      text: 'أحدث مشاريع عقارك',
-                      size: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromRGBO(20, 76, 237, 1.0)),
-
-                ],
-              )),
+              child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                Icon(Icons.arrow_drop_down, size: 40),
+                TextViewCustom(
+                    text: 'أحدث مشاريع عقارك',
+                    size: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(20, 76, 237, 1.0))
+              ])),
           Expanded(
-              child: ListView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: latestProjectsItems?.length,
-                  itemBuilder: (context, index) => Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                          decoration: BoxDecoration(
-                              color: const Color.fromRGBO(20, 76, 237, 1.0),
-                              borderRadius: BorderRadius.circular(15),
-                              image: DecorationImage(
-                                  image: NetworkImage(
-                                      latestProjectsItems?[index].image ?? ''),
-                                  fit: BoxFit.fill)),
-                          margin: const EdgeInsets.all(15),
-                          height: 270,
-                          width: 150
-                          // child: Image.network(homePageBloc.homePageModel!.latestProjects!.items![index].image??'',fit: BoxFit.fill),
-                          ),
-                        Positioned(bottom: 1,child: Container(child:Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            TextViewCustom(text: latestProjectsItems?[index].title,fontWeight: FontWeight.bold,size: 14),
-                            SizedBox(height: 6),
-                            TextViewCustom(text: latestProjectsItems?[index].cityname)
-                          ],
-                        ) ,height: 70,width: 150,decoration: BoxDecoration(border:Border.all(color: Colors.grey.shade400),color: Colors.white, borderRadius: const BorderRadius.vertical(bottom:  Radius.circular(15)),),))
-                    ],
-                  )))
+              child: Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
+                      itemCount: latestProjectsItems?.length,
+                      itemBuilder: (context, index) =>
+                          Stack(alignment: Alignment.center, children: [
+                            Container(
+                                decoration: BoxDecoration(
+                                    color:
+                                        const Color.fromRGBO(20, 76, 237, 1.0),
+                                    borderRadius: BorderRadius.circular(15),
+                                    image: DecorationImage(
+                                        image: NetworkImage(
+                                            latestProjectsItems?[index].image ??
+                                                ''),
+                                        fit: BoxFit.fill)),
+                                margin: const EdgeInsets.all(15),
+                                height: 350,
+                                width: 150),
+                            Positioned(
+                                bottom: 1,
+                                child: Container(
+                                    height: 70,
+                                    width: 150,
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                            color: Colors.grey.shade400),
+                                        color: Colors.white,
+                                        borderRadius:
+                                            const BorderRadius.vertical(
+                                                bottom: Radius.circular(15))),
+                                    child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          TextViewCustom(
+                                              text: latestProjectsItems?[index]
+                                                  .title,
+                                              fontWeight: FontWeight.bold,
+                                              size: 14),
+                                          const SizedBox(height: 6),
+                                          TextViewCustom(
+                                              text: latestProjectsItems?[index]
+                                                  .cityname)
+                                        ])))
+                          ]))))
         ]));
   }
 }
--- lib/module/home_page/ui/widgets/units_items_widget.dart
+++ lib/module/home_page/ui/widgets/units_items_widget.dart
@@ -17,7 +17,10 @@ class UnitsItemsWidget extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Icon(Icons.arrow_drop_down,size: 40,),
+                  Icon(
+                    Icons.arrow_drop_down,
+                    size: 40,
+                  ),
                   TextViewCustom(
                       text: 'وحدات عقارية مقترحة',
                       size: 16,
@@ -26,40 +29,64 @@ class UnitsItemsWidget extends StatelessWidget {
                 ],
               )),
           Expanded(
-              child: ListView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: item.length,
-                  itemBuilder: (context, index) => Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  image: NetworkImage(item[index].photourl ?? ''),
-                                  fit: BoxFit.fill),
-                              color: const Color.fromRGBO(20, 76, 237, 1.0),
-                              borderRadius: BorderRadius.circular(15)),
-                          margin: const EdgeInsets.all(15),
-                          height: 190,
-                          width: MediaQuery.sizeOf(context).width / 1.3),
-                      Positioned(bottom: 1,child: Container(padding: EdgeInsets.all(10),child:Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Row(
-                            children: [
-                              TextViewCustom(text: item[index].pricetext,color:const Color.fromRGBO(20, 76, 237, 1.0)),
-                              Spacer(),
-                              TextViewCustom(text: item[index].title,fontWeight: FontWeight.bold,size: 14),
-                            ],
-                          ),
-                          SizedBox(height: 6),
-                          TextViewCustom(text: item[index].screenname)
-                        ],
-                      ) ,height: 70,width: MediaQuery.sizeOf(context).width / 1.3,decoration: BoxDecoration(border:Border.all(color: Colors.grey.shade400),color: Colors.white, borderRadius: const BorderRadius.vertical(bottom:  Radius.circular(15)),),))
-                    ],
-                  )))
+              child: Directionality(
+            textDirection: TextDirection.rtl,
+            child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                itemCount: item.length,
+                itemBuilder: (context, index) => Stack(
+                      textDirection: TextDirection.ltr,
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                        item[index].photourl ?? ''),
+                                    fit: BoxFit.fill),
+                                color: const Color.fromRGBO(20, 76, 237, 1.0),
+                                borderRadius: BorderRadius.circular(15)),
+                            margin: const EdgeInsets.all(15),
+                            height: 190,
+                            width: MediaQuery.sizeOf(context).width / 1.3),
+                        Positioned(
+                            bottom: 1,
+                            child: Container(
+                              padding: const EdgeInsets.all(10),
+                              height: 70,
+                              width: MediaQuery.sizeOf(context).width / 1.3,
+                              decoration: BoxDecoration(
+                                  border:
+                                      Border.all(color: Colors.grey.shade400),
+                                  color: Colors.white,
+                                  borderRadius: const BorderRadius.vertical(
+                                      bottom: Radius.circular(15))),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children: [
+                                      TextViewCustom(
+                                          text: item[index].title,
+                                          fontWeight: FontWeight.bold,
+                                          size: 14),
+                                      const Spacer(),
+                                      TextViewCustom(
+                                          text: item[index].pricetext,
+                                          color: const Color.fromRGBO(
+                                              20, 76, 237, 1.0)),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 6),
+                                  TextViewCustom(text: item[index].screenname)
+                                ],
+                              ),
+                            ))
+                      ],
+                    )),
+          ))
         ]));
   }
 }
